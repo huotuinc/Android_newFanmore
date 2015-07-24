704d1ed4ea1cdb9358c88e9e7c752ae2fbcaf452
jxdong1013 <jxdong1013@163.com>
2015-07-24 09:33:27 +0800

    fix momery recycle app crash bug

--- src/cy/com/morefan/util/L.java
+++ src/cy/com/morefan/util/L.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class L {
 	public static boolean isDebug = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
-	private static final String TAG = "SFriend";
+	private static final String TAG = "cy.com.morefan";
 
 	// 下面四个是默认tag的函数
 	public static void i(String msg) {
